bb7083cd7b929d9066dfae6701262ef33225a6d6
Jerry Goguette <jerry_h16@hotmail.com>
2020-08-08 02:30:46 -0400

    Added Emerald AI and RTS Scifi Assets, Basic Middle Mech AI Config

---
+++ Assets/Scripts/Core/Unit.cs
@@ -0,0 +1,69 @@
+﻿using EmeraldAI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTS.Core
+{
+    public class Unit : MonoBehaviour
+    {
+        EmeraldAISystem emeraldAI;
+
+        private string id;
+        private bool selected = false;
+
+        [Serializable]
+        public struct AbilityRangeTransform
+        {
+            public string name;
+            public Transform rangedTransform;
+        }
+        [SerializeField] AbilityRangeTransform[] rangeTransforms;
+
+        private void Awake()
+        {
+            id = UnityEditor.GUID.Generate().ToString();
+            emeraldAI = GetComponent<EmeraldAISystem>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        public void OnAbilityUsed()
+        {
+            foreach (AbilityRangeTransform abilityRangeTransform in rangeTransforms)
+            {
+                if (abilityRangeTransform.name == emeraldAI.m_EmeraldAIAbility.name)
+                {
+                    emeraldAI.RangedAttackTransform = abilityRangeTransform.rangedTransform;
+                    break;
+                }
+            }
+        }
+
+        public string GetID()
+        {
+            return id;
+        }
+
+        public bool IsSelected()
+        {
+            return selected;
+        }
+
+        public void SetSelected(bool selected)
+        {
+            this.selected = selected;
+        }
+    }
+}
---
+++ Assets/Scripts/Core/UnitManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTS.Core
+{
+    public class UnitManager : MonoBehaviour
+    {
+        List<Unit> units;
+
+        private void Awake()
+        {
+            units = new List<Unit>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
